6828b84f16bc03ac0ca3f623b284be8905578c83
Ben Kindon <benkindon@gmail.com>
2024-11-01 17:53:09 +1100

    General theming changes. Fixed bugs when adding a table and placing an order. Re-added product view functionality to manager page

--- app/manager.jsx
+++ app/manager.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   SafeAreaView,
   Alert,
-  Image
+  Image,
+  ScrollView,
 } from 'react-native';
 
 import { useRouter } from 'expo-router';
-import { Button, Text, IconButton, MD3Colors } from 'react-native-paper';
+import { Button, Text, DataTable, List } from 'react-native-paper';
 import { connection } from '../config/config.json';
 import SearchModal from '../components/modals/searchModal';
 import NewProduct from '../components/modals/newProduct';
@@ -37,10 +38,45 @@ const Manager = () => {
   
 
   const Separator = () => <View style={styles.separator} />;
-  //I had to define the data types for each property of Product to make the typescript happy
+  
+  //Get all products
+  const [products, setProducts] = useState([]); 
+  async function PopulateProducts() {
+    try {
+      const response = await fetch(`${connection}/products`, {
+        method: 'GET',
+      });
 
+      if (!response.ok) {
+        const error = await response.json();
+        const errorMessage = typeof error.msg === 'string' ? error.msg : 'Unexpected error';
+        Alert.alert('Error', errorMessage);
+        return;
+      }
 
-  const [products, setProducts] = useState([]); //Explicitly pointing the useState to the array, type AND allowing it to be null :S  
+      const data = await response.json();
+
+      if (data.success && Array.isArray(data.msg)) {
+        setProducts(data.msg);
+      } else {
+        const errorMessage = typeof data.msg === 'string' ? data.msg : 'No products found';
+        Alert.alert('Error', errorMessage);
+      }
+    } catch (err) {
+      console.error('Search error:', err);
+      Alert.alert('Error', 'Something went wrong');
+    }
+  }
+
+  //Handle options dropdown
+  const [expanded, setExpanded] = useState({});
+
+  const handlePress = (productId) => {
+    setExpanded(prevState => ({
+      ...prevState,
+      [productId]: !prevState[productId]
+    }));
+  };
 
   // Handle Add Product
   const handleAddProduct = async (newProduct) => { 
@@ -130,6 +166,11 @@ const Manager = () => {
   // Handle Delete Product 
   const handleDeleteProduct = async (id) => { console.log(`Deleting ID: ${id}`)};
 
+  // On Load:
+  useEffect(() => {
+    PopulateProducts();
+    }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Header title={"Manager"}
@@ -137,113 +178,95 @@ const Manager = () => {
         username={null}/>
       <Separator />
       <View style={styles.body}>
-        <View style={{flexDirection: 'row', flex:1}}>
-          <View style={styles.mainContainer}>
-            <Text variant="displaySmall">Product Details</Text>
-            {/* only show this bit if there are products */}
-            {products && products.length > 0 && (
-            <View>
-              {products.map((product, index) => (
-                product && (
-                  <View key={index} style={styles.table}>
-                    <View style={styles.tableRow}>
-                      <Text style={styles.tableCell}>Name:</Text>
-                      <Text style={styles.tableCell}>{product.name}</Text>
-                    </View>
-                    <View style={styles.tableRow}>
-                      <Text style={styles.tableCell}>Price:</Text>
-                      <Text style={styles.tableCell}>${product.price}</Text>
-                    </View>
-                    <View style={styles.tableRow}>
-                      <Text style={styles.tableCell}>Course:</Text>
-                      <Text style={styles.tableCell}>{product.course}</Text>
-                    </View>
-                    <View style={styles.tableRow}>
-                      <Text style={styles.tableCell}>Image:</Text>
-                      {product.image ? (
-                        <Image source={{ uri: product.image }} style={{ width: 100, height: 100 }} />
-                      ) : (
-                        <Text>No image available</Text>
-                      )}
-                    </View>
-                    {product.options && product.options.length > 0 && (
-                    <View style={styles.tableRow}>
-                      <Text style={styles.tableCell}>Options:</Text>
-                      {product.options.map((option, optIndex) => (
-                        option && option.item && (
-                          <Text key={optIndex} style={styles.tableCell}>
-                            {option.item.name} - ${option.item.price}
-                          </Text>
-                        )
-                      ))}
-                    </View>
-                  )}
+          <View style={styles.managerMainContainer}>
+            <DataTable style={{flex:1}}>
+              <DataTable.Header>
+                <DataTable.Title>Name</DataTable.Title> 
+                <DataTable.Title>Price</DataTable.Title>  
+                <DataTable.Title>Course</DataTable.Title>
+                <DataTable.Title>Image</DataTable.Title>    
+                <DataTable.Title>Options</DataTable.Title>              
+              </DataTable.Header>
+              <ScrollView contentContainerStyle={{flexDirection:'column'}}>
+              {products && Array.isArray(products) && products.length > 0 ? (
+               products.map((product) => (
+                <DataTable.Row key={product._id}>
+                  <DataTable.Cell>{product.name}</DataTable.Cell>
+                  <DataTable.Cell>${product.price}</DataTable.Cell>
+                  <DataTable.Cell>{product.course}</DataTable.Cell>
+                  <DataTable.Cell>{product.image}</DataTable.Cell>
+                  <DataTable.Cell>
+                    {product.options && product.options.length > 0 ? (
+                      <List.Section title="Options" style={[{flexDirection:'row',paddingVertical:0, paddingHorizontal:0, marginHorizontal:0, marginVertical:0}]}>
+                        <List.Accordion
+                          style={[{paddingVertical:0, paddingHorizontal:0, marginHorizontal:0, marginVertical:0}]}
+                          expanded={expanded[product._id] || false}
+                          onPress={() => handlePress(product._id)}
+                        >
+                          {product.options.map((option, index) => (
+                            <List.Item
+                              key={index}
+                              title={option.name}
+                              description={`Price: $${option.price}`}
+                            />
+                          ))}
+                        </List.Accordion>
+                      </List.Section>
+                    ) : null} 
+                  </DataTable.Cell>
+                </DataTable.Row>
+                  ))
+                  ) : null}
+              </ScrollView>
+            </DataTable>
+          </View>
+            <View style={[styles.managerButtonContainer, {flexDirection:'row'}]}>
+              <View style={{flex:1}}>
+                <View style={styles.buttonRow}>
+                  <Button style={[styles.squareButton, styles.managerButton]}
+                      mode="contained"
+                      icon="plus"
+                      onPress={showNewProductModal}>              
+                      Add
+                  </Button>
+                  <Button style={[styles.squareButton, styles.managerButton]}
+                      mode="contained"
+                      icon="pencil">              
+                      Edit
+                  </Button>
+                </View>
+                <View style={styles.buttonRow}>
+                  <Button style={[styles.squareButton, styles.managerButton]}
+                        mode="contained"
+                        icon="magnify"
+                        onPress={showSearchModal}>              
+                        Lookup
+                    </Button>
+                    <Button style={[styles.squareButton, styles.managerButton]}
+                        mode="contained"
+                        icon="delete"
+                        onPress={showDeleteModal}>              
+                        Delete
+                    </Button>
                   </View>
-                )
-              ))}
-            </View>
-            )}      
-          </View>
-          <View style={styles.rightContainer}>
-            <View style={styles.numpadContainer}>
-              <Text>Numpad</Text>
-            </View>
-            <View style={[styles.buttonContainer]}>
-              <View style={styles.buttonRow}>
-                <IconButton style={styles.squareButton}
-                  icon="plus-box"
-                  iconColor='rgb(229 220 200)'
-                  containerColor='rgb(156, 64, 77)'
-                  mode="contained"
-                  size={30}
-                  onPress={showNewProductModal}
-                />
-                <IconButton style={styles.squareButton}
-                  icon="pencil"
-                  iconColor='rgb(229 220 200)'
-                  containerColor='rgb(156, 64, 77)'
-                  mode="contained"
-                  size={30}
-                  onPress={() => console.log('Edit not implemented')}
-                />
               </View>
-              <View style={styles.buttonRow}>
-                <IconButton style={styles.squareButton}
-                  icon="magnify"
-                  iconColor='rgb(229 220 200)'
-                  containerColor='rgb(156, 64, 77)'
-                  mode="contained"
-                  size={30}
-                  onPress={showSearchModal}
-                />
-                <IconButton style={styles.squareButton}
-                  icon="delete"
-                  iconColor='rgb(229 220 200)'
-                  containerColor='rgb(156, 64, 77)'
-                  mode="contained"
-                  size={30}
-                  onPress={showDeleteModal}
-                />
-              </View>
-              <View style={styles.buttonRow}>
-                <Button style={[styles.squareButton, styles.wideButton]}
-                  mode="contained"
-                  icon="poll">              
-                  Report
-                </Button>
-                <IconButton style={styles.roundButton}
-                  icon="home-roof"
-                  iconColor='rgb(229 220 200)'
-                  containerColor='rgb(156, 64, 77)'
-                  mode="contained"
-                  size={30}
-                  onPress={() => router.push('/')}
-                />
+              <View style={{flex:1}}>
+                <View style={styles.buttonRow}>
+                  <View style={styles.displayPortal}>
+                    <Text variant='bodySmall'>Daily Total:</Text>
+                    <Text variant='labelLarge'>$ null</Text>
+                  </View>
+                  </View>
+                <View style={styles.buttonRow}>
+                  <Button style={[styles.squareButton, styles.managerButton, {width:'98%'}]}
+                    mode="contained"
+                    icon="poll">              
+                    Report
+                  </Button>
+                </View>
               </View>
             </View>
-          </View>
         </View>
-      </View>
       
       {/* Modals */}
       <SearchModal visible={searchModalVisible} onDismiss={hideSearchModal} onSearch={handleSearch} />
