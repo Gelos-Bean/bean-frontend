f39a6a0fa0d577853dfdfad9adb0c264071bc601
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-11-02 14:02:25 +1100

    Added AddTableModal to Tab view

--- app/tabs/ViewAllTabs.jsx
+++ app/tabs/ViewAllTabs.jsx
@@ -1,62 +1,66 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { connection } from '../../config/config.json';
+
 import TabBtnMenu from '../../components/tab/TabBtnMenu.jsx';
+import AddTableModal from '../../components/modals/addTable.jsx';
 
 import styles from '../../styles/posStyles.js';
 
 
 export default function ViewAllTabs({ onSelectTab }) {
 
-    const headers = ["Tab", "Arrival", "PAX", "Total"];
-    const [tables, setTables] = useState([]);
+  const headers = ["Tab", "Arrival", "PAX", "Total"];
+  const [tables, setTables] = useState([]);
+  const [newTable, setNewTable] = useState({});
+  const [viewTableModal, setViewTableModal] = useState(false);
 
 
-    useEffect(() => {
-        fetchData();
-      },[]);
+  useEffect(() => {
+      fetchData();
+    },[tables]);
 
       
-    async function fetchData(){ 
-        
-      try { 
-        const response = await fetch(`${connection}/tables`);
-        const tabs = await response.json();
-  
-  //------> Create functionality to display this error to user
-        if (!tabs.success) {
-          return console.log(`Error: ${tabs.msg}`);
-        }
-            
-        setTables(tabs.msg);
-    
-      } catch(err) {
-        console.log(err);
-      } 
-    }
+  async function fetchData(){ 
+      
+    try { 
+      const response = await fetch(`${connection}/tables`);
+      const tabs = await response.json();
 
-    function formatTime(formatDate) {
-      const date = new Date(formatDate);
-      const options = { hour: '2-digit', minute: '2-digit', hour12: true };
-      return date.toLocaleTimeString([], options);
-    }
-
-    function sortBy(title) {
-
-      let sortedData = [...tables];
-
-      if (title === "PAX") {
-          sortedData.sort((a, b) => (a.pax > b.pax ? 1 : -1));
-      } else if (title === "Total") {
-          sortedData.sort((a, b) => (a.total > b.total ? 1 : -1));
-      } else if (title === "Tab") {
-            sortedData.sort((a, b) => (a.tableNo > b.tableNo ? 1 : -1));
-      } else if (title === "Arrival") {
-          sortedData.sort((a, b) => (new Date(a.openedAt) > new Date(b.openedAt) ? 1 : -1));
+//------> Create functionality to display this error to user
+      if (!tabs.success) {
+        return console.log(`Error: ${tabs.msg}`);
       }
+          
+      setTables(tabs.msg);
+  
+    } catch(err) {
+      console.log(err);
+    } 
+  }
 
-      setTables(sortedData);
+  function formatTime(formatDate) {
+    const date = new Date(formatDate);
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    return date.toLocaleTimeString([], options);
+  }
+
+  function sortBy(title) {
+
+    let sortedData = [...tables];
+
+    if (title === "PAX") {
+        sortedData.sort((a, b) => (a.pax > b.pax ? 1 : -1));
+    } else if (title === "Total") {
+        sortedData.sort((a, b) => (a.total > b.total ? 1 : -1));
+    } else if (title === "Tab") {
+          sortedData.sort((a, b) => (a.tableNo > b.tableNo ? 1 : -1));
+    } else if (title === "Arrival") {
+        sortedData.sort((a, b) => (new Date(a.openedAt) > new Date(b.openedAt) ? 1 : -1));
+    }
+
+    setTables(sortedData);
 
   }
 
@@ -100,10 +104,18 @@ export default function ViewAllTabs({ onSelectTab }) {
       </View>
       <View style={styles.rightContainer}>
           <View style={{flex: 3}}>
-            <TabBtnMenu />
+            <TabBtnMenu 
+              setNewTable={setNewTable}
+              setViewTableModal={setViewTableModal}
+            />
           </View>
       </View>
+      <AddTableModal    
+        visible={viewTableModal}
+        setVisibility={setViewTableModal}
+        onAdd={setNewTable}
+      />
     </>
-  )
-}
+  );
+};
 
--- components/tab/TabBtnMenu.jsx
+++ components/tab/TabBtnMenu.jsx
@@ -1,9 +1,44 @@
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Alert } from 'react-native';
 import { Button, IconButton, Text } from 'react-native-paper';
+import connection from '../../config/config.json';
 import styles from '../../styles/posStyles.js';
 
 
-export default function TabBtnMenu() {
+export default function TabBtnMenu({ setViewTableModal }) {
+ 
+  function handleTableModal() {
+    setViewTableModal(true);
+  }
+
+  async function addNewTable(tTableNo, tPax, tLimit) {
+    try { 
+      const createTable = {
+        tableNo: tTableNo,
+        pax: tPax,
+        limit: tLimit    
+      }
+
+      const response = await fetch(`${connection}/add-table`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(createTable)
+      });
+
+      if(!response.success) {
+        console.log(response.msg);
+        return Alert.alert(response.msg);
+      }
+      
+      console.log(response._id);
+
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
   return (
       <View style={styles.buttonContainer}>
           <View style={styles.buttonRow}>
@@ -11,6 +46,13 @@ export default function TabBtnMenu() {
               <Text variant='bodySmall'>Tab:</Text>
               <Text variant='labelLarge'>{null}</Text>
             </View>
+            <IconButton style={[styles.squareButton, {}]}
+                    icon="plus"
+                    mode="contained"
+                  selected={true}
+                    size={30}
+                    onPress={handleTableModal}
+                  />
             <View style={styles.buttonText}>
               <IconButton style={styles.squareButton}
                 icon="broom"
