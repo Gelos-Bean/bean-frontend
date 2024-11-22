110431fde679a514770dd0af86e3b9a296bdaccb
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-11-22 18:04:44 +1100

    better error handling

--- app/context/AuthContext.jsx
+++ app/context/AuthContext.jsx
@@ -1,8 +1,11 @@
 import React, { createContext, useState } from 'react';
 import { Alert } from 'react-native';
 import { connection } from '../../config/config.json';
-import ShowError from '../../components/ShowError.jsx';
 import { jwtDecode } from 'jwt-decode';
+
+import ShowError from '../../components/ShowError.jsx';
+import { withTimeout } from '../../components/WithTimeout.jsx';
+
 
 export const AuthContext = createContext(); 
 
@@ -13,53 +16,32 @@ export const AuthProvider = ({ children }) => {
         authenticated: null
     });
 
-    const login = async (username, pin, onSuccess, onError) => {
-        if (!username || !pin)
-            return Alert.alert('Missing Details', 'Both username and pin are required to log in.');
-    
-        const credentials = { username, pin };
-    
-        try {
-            const response = await fetch(`${connection}/users/login`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(credentials),
-            });
-    
-            if (!response.ok) {
-                const error = await response.json();
-                Alert.alert('Login Failed', error?.msg || 'Incorrect username or pin.');
-
-                if (onError) 
-                    return onError(error);
-            }
-    
-            const data = await response.json();
-            
-            if (data.success) {
-                const decodeToken = jwtDecode(data.token);
-
-                if (decodeToken) {
-                    setAuthState({
-                        user: decodeToken,
-                        token: data.token,
-                        authenticated: true,
-                    });
-                }
-            
-            if (onSuccess) onSuccess(data);
-            } else {
-                ShowError(data.msg);
-                
-                if (onError) 
-                    return onError(data.msg);
-            }
-        } catch (err) {
-            ShowError('There was a problem logging in. Please check your network connection and try again.');
-            console.log(err.message);
-            if (onError) 
-                return onError(err);
+    const login = async (username, pin) => {
+        if (!username || !pin) {
+            return Alert.alert('Missing Details', 'Both username and pin are required to log in.'); 
         }
+        
+        const credentials = { username, pin };
+        const response = await withTimeout(fetch(`${connection}/users/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials),
+        }), 5000);
+    
+        const data = await response.json();
+
+        if (!data.success)
+            return Alert.alert('Login Failed', data?.msg || 'Incorrect username or pin.');
+
+        const decodeToken = jwtDecode(data.token);
+        if(!decodeToken)
+            return Alert.alert('Authentication failed');
+        
+        setAuthState({ 
+            user: decodeToken, 
+            token: data.token, 
+            authenticated: true 
+        });
     };
 
     const logout = () => {
@@ -69,9 +51,10 @@ export const AuthProvider = ({ children }) => {
         authentication: null
     })};
 
+    console.log(authState);
     return (
         <AuthContext.Provider 
-                    value={{ authState, login, logout }}>
+                value={{ authState, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
